4a8bf6ea37772de49763e40c33cb1ba80af3f20f
Daniel Hones <danielhones@gmail.com>
2016-08-13 00:41:24 -0400

    added nltk to install_requires in setup.py, it appears to be the only dependendcy

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(name='textteaser',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      # TODO: determine dependencies, make requirements.txt and fill out this line:
-      # install_requires=[]
+      install_requires=['nltk'],
 )
