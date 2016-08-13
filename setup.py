44e42bcf8253875573334fa53726c6d066e9da17
Daniel Hones <danielhones@gmail.com>
2016-08-13 00:35:58 -0400

    Fixed some prints for Python3 compatibility, wrote setup.py and MANIFEST.in for easy installing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='textteaser',
+      url='https://github.com/DataTeaser/textteaser',
+      license='MIT',
+      version='0.0.2',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      # TODO: determine dependencies, make requirements.txt and fill out this line:
+      # install_requires=[]
+)
